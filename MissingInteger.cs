ae75653d2f4f29f4f65a7bbb1962cd721115cc72
hilk <115804595+hayriyedurgun@users.noreply.github.com>
2023-01-11 17:53:18 +0300

    MissingInteger solved

---
+++ MissingInteger.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityExercises
+{
+    public class MissingInteger
+    {
+        public int solution(int[] A)
+        {
+            var max = 0;
+            var list = new HashSet<int>(A.Length);
+            foreach (var item in A)
+            {
+                if (item > 0)
+                {
+                    list.Add(item);
+                    if (item > max)
+                    {
+                        max = item;
+                    }
+                }
+            }
+
+            if (max == 0)
+            {
+                return 1;
+            }
+
+            for (int i = 1; i <= max + 1; i++)
+            {
+                //contains's complexity o(1) in hashset!
+                if(!list.Contains(i))
+                {
+                    return i;
+                }
+            }
+
+            return 1;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,8 +9,19 @@ namespace CodilityExercises
     {
         static void Main()
         {
-            var maxCounters = new MaxCounters();
-            maxCounters.solution(5, new int[] { 3, 4, 4, 6, 1, 4, 4 });
+            //var maxCounters = new MaxCounters();
+            //maxCounters.solution(5, new int[] { 3, 4, 4, 6, 1, 4, 4 });
+
+            var smallestSet1 = new int[] { 1, 3, 6, 4, 1, 2 };
+            var smallestSet2 = new int[] { 1, 2, 3 };
+            var smallestSet3 = new int[] { 1 * -1, 3 * -1 };
+            var smallestSet4 = new int[] { 1 * -1, 0,1, 3 };
+            var smallestInt = new MissingInteger();
+
+            var result1 = smallestInt.solution(smallestSet1);
+            var result2 = smallestInt.solution(smallestSet2);
+            var result3 = smallestInt.solution(smallestSet3);
+            var result4 = smallestInt.solution(smallestSet4);
         }
     }
 }
