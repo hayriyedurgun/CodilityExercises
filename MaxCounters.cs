36ec94df691d8eb677e6a729a6c0007c4e58a923
hilk <115804595+hayriyedurgun@users.noreply.github.com>
2023-01-11 16:42:32 +0300

    initial commit, maxcounter exercise

---
+++ MaxCounters.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodilityExercises
+{
+    public class MaxCounters
+    {
+        private int[] _counterArray;
+
+        public int[] solution(int N, int[] A)
+        {
+            _counterArray = new int[N];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] < N)
+                {
+                    Increment(A[i]);
+                }
+                else
+                {
+                    SetMax();
+                }
+            }
+
+            return _counterArray;
+        }
+
+        private void Increment(int index)
+        {
+            _counterArray[index - 1]++;
+        }
+
+        private void SetMax()
+        {
+            var max = _counterArray.Max();
+            for (int i = 0; i < _counterArray.Length; i++)
+            {
+                _counterArray[i] = max;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodilityExercises
+{
+    class Program
+    {
+        static void Main()
+        {
+            var maxCounters = new MaxCounters();
+            maxCounters.solution(5, new int[] { 3, 4, 4, 6, 1, 4, 4 });
+        }
+    }
+}
